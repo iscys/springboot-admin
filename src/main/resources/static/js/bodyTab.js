2eb543885cf42cc57f2e8fe271b86597418677d5
iscys <iscys>
2019-05-17 22:58:27 +0800

    d

--- src/main/resources/static/js/bodyTab.js
+++ src/main/resources/static/js/bodyTab.js
@@ -1,7 +1,7 @@
 
 var tabFilter,menu=[],liIndex,curNav,delMenu;
 layui.define(["element","jquery"],function(exports){
-	var element = layui.element(),
+	var element = layui.element,
 		$ = layui.jquery,
 		layId,
 		Tab = function(){
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,7 +1,7 @@
 layui.config({
 	base : "js/"
 }).use(['form','layer'],function(){
-	var form = layui.form(),
+	var form = layui.form,
 		layer = parent.layer === undefined ? layui.layer : parent.layer,
 		$ = layui.jquery;
 	//video背景
