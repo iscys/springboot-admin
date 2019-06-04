fe43e52b0df4a2e8ed80c8eaa5d201b3d6095c5b
Administrator <asa>
2019-06-04 13:27:42 +0800

    s

---
+++ src/main/java/com/project/model/OilCardTicket.java
@@ -0,0 +1,20 @@
+package com.project.model;
+
+public class OilCardTicket {
+
+    private String id;
+    private String uid;
+    private String order_id;
+    private String order_sn;
+    private String realname;
+    private String mobile;
+    private String prov;
+    private String city;
+    private String dist;
+    private String address;
+    private String express_name;
+    private String express_mobile;
+    private String express_time;
+    private String status;
+    private String create_time;
+}
