8248cf071ecb74b15eb8b047fb4302be27c54852
iscys <iscys>
2019-06-08 13:16:56 +0800

    d

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,11 +1,11 @@
 var $,tab,skyconsWeather;
 layui.config({
 	base : "../js/"
-}).use(['bodyTab','form','element','layer','jquery'],function(){
+}).use(['bodyTab','form','element','layer'],function(){
 	var form = layui.form,
 		layer = layui.layer,
 		element = layui.element;
-		$ = layui.jquery;
+		//$ = layui.jquery;
 		tab = layui.bodyTab({
 			openTabNum : "50",  //最大可打开窗口数量
 			url : "/sys/menu" //获取菜单json地址
