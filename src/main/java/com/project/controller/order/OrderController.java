7a920499c2791d3cd4f59289e4894a5e57d1decb
iscys <iscys>
2019-06-15 22:47:46 +0800

    ok

--- src/main/java/com/project/controller/order/OrderController.java
+++ src/main/java/com/project/controller/order/OrderController.java
@@ -1,6 +1,8 @@
 package com.project.controller.order;
 
 import com.project.controller.BaseController;
+import com.project.utils.DataPager;
+import com.project.utils.PageData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,7 +13,11 @@ public class OrderController extends BaseController {
 
     @RequestMapping("/index")
     public ModelAndView index(){
+        DataPager dataPager=new DataPager(1,890,50);
+        dataPager.setRecords(1);
+        dataPager.setFormId("Form");
         ModelAndView mv = this.getModelAndView();
+        mv.addObject("dataPager",dataPager);
         mv.setViewName("page/order/orderList");
         return mv;
     }
