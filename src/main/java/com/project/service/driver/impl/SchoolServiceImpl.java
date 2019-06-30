823b77e8baa15ee8c7fc3a3bb1f74fa31f8bffae
iscys <iscys>
2019-06-30 22:06:58 +0800

    ok

--- src/main/java/com/project/service/driver/impl/SchoolServiceImpl.java
+++ src/main/java/com/project/service/driver/impl/SchoolServiceImpl.java
@@ -5,7 +5,9 @@ import com.project.mapper.admin.DriverHomeMapper;
 import com.project.model.Const;
 import com.project.model.ResultObject;
 import com.project.model.school.Album;
+import com.project.model.school.Mark;
 import com.project.model.school.SchoolModel;
+import com.project.service.admin.MarkService;
 import com.project.service.driver.SchoolService;
 import com.project.utils.Page;
 import com.project.utils.PageData;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,6 +26,8 @@ public class SchoolServiceImpl implements SchoolService {
     
     @Autowired
     private DriverHomeMapper homeMapper;
+    @Autowired
+    private MarkService markService;
 
     /**
      * 查看驾校具体信息
@@ -63,7 +68,25 @@ public class SchoolServiceImpl implements SchoolService {
         int pageSize1 = page.getPageSize();
         pd.put("startIndex",startIndex);
         pd.put("pageSize",pageSize1);
-        List<HashMap<String,String>> lists =homeMapper.getHomeList(pd);
+        List<HashMap<String,Object>> lists =homeMapper.getHomeList(pd);
+        for(HashMap<String,Object> map :lists){
+            Object mark=map.get("mark");
+            List<String> strL =new ArrayList<>();
+            if(null !=mark){
+                String marks = mark.toString();
+                String[] arr = marks.split(",");
+                for(String m:arr){
+                    Mark mark1 =new Mark();
+                    mark1.setId(m);
+                    Mark markDetail = markService.getMarkDetail(mark1);
+                    if(null !=markDetail) {
+                        strL.add(markDetail.getMark());
+                    }
+
+                }
+            }
+            map.put("tag",strL);
+        }
         HashMap<String,Object> res =new HashMap<String,Object>();
         res.put("pageNum",Integer.valueOf(pageNum));//c传过来到页数
         res.put("totalPage",page.getTotalPage());//总页数
