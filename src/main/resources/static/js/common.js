b3c6271825365d64abbcfd36d12270106027a011
iscys <iscys>
2019-06-30 22:10:44 +0800

    ok

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,21 +1,4 @@
 
-layui.use(['form','laydate', 'laypage', 'layer', 'table', 'carousel', 'element', 'slider'], function() {
-	var laydate = layui.laydate //日期
-		, laypage = layui.laypage //分页
-		, layer = layui.layer //弹层
-		, table = layui.table //表格
-		, element = layui.element //元素操作
-		, slider = layui.slider //滑块
-		,form = layui.form
-
-	/**
-	 laydate.render({
-            elem: '#teacher_apply'
-        });
-	 **/
-
-
-});
 /**
  * 判断，弹框
  * @constructor
