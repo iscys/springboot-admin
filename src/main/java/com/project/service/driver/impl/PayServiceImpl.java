82e9bfb1610d1521bf97f855085cb2d145f70c42
iscys <iscys>
2019-07-01 23:24:16 +0800

    ok

--- src/main/java/com/project/service/driver/impl/PayServiceImpl.java
+++ src/main/java/com/project/service/driver/impl/PayServiceImpl.java
@@ -2,21 +2,27 @@ package com.project.service.driver.impl;
 
 import com.github.binarywang.wxpay.bean.notify.WxPayOrderNotifyResult;
 import com.github.binarywang.wxpay.bean.request.BaseWxPayRequest;
+import com.github.binarywang.wxpay.bean.request.WxPayRefundRequest;
+import com.github.binarywang.wxpay.service.WxPayService;
 import com.project.config.ConfigProperties;
 import com.project.mapper.admin.ApplyMapper;
 import com.project.mapper.admin.OrderMapper;
 import com.project.model.school.Apply;
 import com.project.model.school.ErrorModel;
 import com.project.model.school.Order;
+import com.project.model.school.ThirdResult;
 import com.project.service.driver.OrderErrorService;
 import com.project.service.driver.PayService;
 import com.project.utils.DateUtils;
+import com.project.utils.GsonUtils;
 import com.project.utils.HttpUtils;
+import com.project.utils.ToolsUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 
@@ -33,6 +39,10 @@ public class PayServiceImpl implements PayService {
     OrderErrorService error;
     @Autowired
     private ConfigProperties properties;
+    @Autowired
+    private WxPayService wxPayService;
+    @Autowired
+    private OrderErrorService errorService;
     /**
      * 微信支付回调通知
      * @param notifyResult
@@ -62,6 +72,9 @@ public class PayServiceImpl implements PayService {
                 Apply apply =new Apply();
 
                 apply.setId(Integer.valueOf(order.getApply_id()));
+
+
+                //通知第三方进行学员信息的录入
                 Apply applys =applyMapper.getDeatilApply(apply);
 
                 HashMap<String,String> map  =new HashMap<>();
@@ -75,13 +88,36 @@ public class PayServiceImpl implements PayService {
                 map.put("applydate",applys.getApplydate());
                 map.put("orgcode",applys.getOrgcode());
                 map.put("photo",apply.getPhoto());
+
                 String result = HttpUtils.INSTANCE.doPost(properties.getApplyUrl(), map);
-                System.err.println(result);
 
+                ThirdResult thirdResult = GsonUtils.fromJson(result, ThirdResult.class);
+
+
+
+
+                //{Code :1 ,Message:success}
+                if(StringUtils.isEmpty(thirdResult.getCode()) || !thirdResult.isSuccess()){
+                    logger.error("学员信息插入第三方库失败，信息：{}",result);
+                    //todo refund 第三方库插入失败，执行退款操作
+                        logger.info("---微信退款--");
+                        WxPayRefundRequest refund = new WxPayRefundRequest();
+                        refund.setOutTradeNo(outTradeNo);
+                        refund.setTotalFee(wxFee);
+                        refund.setRefundFee(wxFee);
+                        refund.setOutRefundNo(DateUtils.getTimeInMillis()+ ToolsUtils.sixCode());
+                        wxPayService.refund(refund);
+
+
+                }else{
+                    //success
+                    try {
+                        ErrorModel model = new ErrorModel(outTradeNo, "微信支付成功,并且学员信息录入成功", null);
+                        errorService.saveErrorLog(model);
+                    }catch (Exception e){}
+
+                }
             }
-
-
-
 
 
 
