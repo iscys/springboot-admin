fffdcb3eb295298b478049e80109571a0b5ffd39
iscys <iscys>
2019-07-04 18:10:52 +0800

    up

---
+++ src/main/java/com/project/service/admin/SubjectOrderService.java
@@ -0,0 +1,11 @@
+package com.project.service.admin;
+
+import com.project.utils.DataPager;
+import com.project.utils.PageData;
+
+
+public interface SubjectOrderService {
+    DataPager getOrderList(PageData pd);
+
+
+}
